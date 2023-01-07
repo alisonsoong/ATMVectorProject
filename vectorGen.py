6fc9976b35144eb3bf02ed1cd0b79a678f20cbac
Alison Soong <alisonsoong@gmail.com>
2023-01-07 00:13:24 -0800

    fixed extra curve

--- vectorGen.py
+++ vectorGen.py
@@ -51,26 +51,31 @@ class VectorFunc:
     def polyDeg_2_2D(self):
         """In the form:
 
-        r(t) = <at+b, ct^2+d>
+        r(t) = <at^2+b, ct^2+d>
 
         Returns:
             np list: xline, yline, endpoint, enddir
         """
         t,x,xp,y,yp = self.ti,self.x,self.xp,self.y,self.yp
         
-        a = xp
-        if t != 0: c = yp/(2*t)
+        if t != 0: 
+            a = xp/(2*t)
+            c = yp/(2*t)
         else: # t is zero, you can do whatever you want
+            a = random.randint(-RAND.SCALE_FACTOR,RAND.SCALE_FACTOR) 
             c = random.randint(-RAND.SCALE_FACTOR,RAND.SCALE_FACTOR) 
-        b = x-a*t
+        b = x-a*pow(t,2)
         d = y-c*pow(t,2)
 
-        xline = a * self.domain + b
+        xline = a * np.power(self.domain, 2) + b
         yline = c * np.power(self.domain, 2) + d
 
-        endpoint = (a*self.tf+b, c*pow(self.tf, 2)+d)
-        enddir = (a,2*c*self.tf)
+        endpoint = (a*pow(self.tf, 2)+b, c*pow(self.tf, 2)+d)
+        enddir = (2*a*self.tf,2*c*self.tf)
         #print(xline,yline,endpoint,enddir)
+        print("deg 2")
+        print("start point: ", (a*pow(t, 2)+b, c*pow(t, 2)+d))
+        print("start direction: ", (2*a*t,2*c*t))
         return xline, yline, endpoint, enddir
 
 
@@ -96,6 +101,9 @@ class VectorFunc:
 
         endpoint = (a*self.tf+b, c*pow(self.tf, 3)+d)
         enddir = (a,3*c*pow(self.tf, 2))
+        print("deg 3")
+        print("start point: ", (a*t+b, c*pow(t, 3)+d))
+        print("start direction: ", (a,3*c*pow(t, 2)))
         return xline, yline, endpoint, enddir
 
     def sinSqrPlusCos(self):
@@ -110,22 +118,29 @@ class VectorFunc:
         
         # redo this
         a = xp
-        b = x-xp*t
+        b = x-a*t
         try:
-            if t == 0:
+            if t==0:
+                print("SWITCH")
                 return self.polyDeg_3_2D()
-            else:
-                d = (4*y-np.tan(t))/(np.tan(t)*np.sin(t) + 4*np.cos(t))
-                c = (yp + d*np.sin(t))/(np.sin(2*t)) 
+           # d = (2*y-yp*np.tan(t))/(np.tan(t)*np.cos(t) + 2*np.cos(t))#(y-yp/np.sin(2*t))/(1/(2*np.cos(t)) + np.cos(t))# (y-np.tan(t)*yp)/(np.tan(t)*np.sin(t) + np.cos(t))# (4*y-np.tan(t))/(np.tan(t)*np.sin(t) + 4*np.cos(t))
+           # c = (yp + d*np.sin(t))/(2*np.sin(t)*np.cos(t)) 
+            c = (yp + y*np.tan(t)) / (np.sin(2*t) + np.tan(t) * np.sin(t) *np.sin(t))
+            d = (y-c*np.sin(t)*np.sin(t)) / (np.cos(t))
         except: 
             # if it doesn't work, just use another one
+            print("SWITCH")
             return self.polyDeg_3_2D()
 
         xline = a * self.domain + b
         yline = c * np.power(np.sin(self.domain), 2) + d * np.cos(self.domain) # we know this is still wrong
 
         endpoint = (a * self.tf + b, c * np.power(np.sin(self.tf), 2) + d * np.cos(self.tf))
-        enddir = (a,c*np.sin(self.tf*t)-d*np.sin(self.tf))
+        enddir = (a,c*np.sin(self.tf*2)-d*np.sin(self.tf))
+        print("curve", t)
+        print(a,b,c,d)
+        print("start point: ", (a * t + b, c * np.power(np.sin(t), 2) + d * np.cos(t)))
+        print("start direction: ", (a,c*np.sin(t*2)-d*np.sin(t)))
         return xline, yline, endpoint, enddir
 
 class GenCircuit:
@@ -143,15 +158,20 @@ class GenCircuit:
 
         for i in range(numPieces):
             zline = np.linspace(5*i, 5*(i+1), 1000)
+            print("starttt point: ", endpoint)
+            print("startt direction: ", enddir)
+            print(5*i)
             curve = VectorFunc(self.randVectorFunc(), endpoint, enddir, zline, (5*i,5*(i+1)))
             xline, yline, endpoint, enddir = curve.gen()
+            print("end point: ", endpoint)
+            print("end direction: ", enddir)
             ax.plot3D(xline, yline, self.zFunc.getWithRange(zline), colors[i%7])
-        
+            
         plt.show()
 
     def randVectorFunc(self):
         r = random.random()
-        if (r < 0.7):
+        if (r < 0.9):
             return CONST.SINSQR_PLUS_COS
         else:
             return CONST.POLY_DEG_2_2D if bool(random.randint(0,1)) else CONST.POLY_DEG_2_2D
